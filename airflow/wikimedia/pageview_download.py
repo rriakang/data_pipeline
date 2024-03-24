6e1727e5dcd57d6ce2da89509dd9d4138f90dba0
rriakang <kangrira123@gmail.com>
2024-03-24 16:36:52 +0900

    revise

---
+++ airflow/wikimedia/pageview_download.py
@@ -0,0 +1,29 @@
+import airflow.utils.dates
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+
+
+dag = DAG(
+    start_data = airflow.utils.dates.days_ago(3),
+    schedule_interval = "hourly",
+)
+
+get_data = BashOperator(
+
+    task_id = "get_data",
+    bash_command=(
+        "curl -o /tmp/wikipageviews.gz"
+        "https://dumps.wikimedia.org/other/pageviews/"
+        "{{ execution_date.year}}/"
+        "{{ execution_date.year}}-"
+        "{{ '{:02}'.format(execution_date.month)}}/"
+        "pageviews-{{ execution_date.year}}"
+        "{{ '{:02}'.format(execution_date.month)}}"
+        "{{ '{:02}'.format(execution_date.day) }}-"
+        "{{ '{:02}'.format(execution_date.hour)}}0000.gz"
+
+    ),
+    dag = dag,
+)
+
