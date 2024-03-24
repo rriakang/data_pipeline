957bde693d111d6e706c34b6f9d8b442f3b6fc33
rriakang <kangrira123@gmail.com>
2024-03-24 15:08:22 +0900

    template

--- airflow/0318/take_data/05_query_with_dates.py
+++ airflow/0318/take_data/05_query_with_dates.py
@@ -6,6 +6,7 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
+# 특정 시간 간격에 대한 이벤트 데이터 가져오기
 fetch_events = BashOperator(
     task_id = "fetch_events",
     bash_command=(
@@ -17,4 +18,21 @@ fetch_events = BashOperator(
     ),
 
     dag = dag,
+),
+
+# 특정 날짜 지점을 위해 템플릿 사용하기
+
+fetch_events = BashOperator(
+    task_id = "fetch_events",
+    bash_command=(
+        "mkdir -p /data && "
+        "curl -o /data/events.json "
+        "http://localhost:5000/events?"
+        "start_date = {{execution_date.strftime('%Y-%m-%d')}}"
+        "&end_date = {{next_execution_date.strftime('%Y-%m-%d')}}"
+    ),
+
+    dag = dag,
+
+
 ),
